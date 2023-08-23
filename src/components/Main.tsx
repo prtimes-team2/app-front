e5520132a34f2ea6db370ba7375f596802963092
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-08-23 11:33:32 +0900

    feat: create route

---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import { Router } from '../routes/Router';
+
+export const Main = () => {
+  return (
+    <main>
+      <Router />
+    </main>
+  );
+};
---
+++ src/routes/Router.tsx
@@ -0,0 +1,19 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Loading } from '../components/Loading';
+
+const Top = lazy(() => import('../pages/Top'));
+const Error = lazy(() => import('../pages/Error'));
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Top />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
