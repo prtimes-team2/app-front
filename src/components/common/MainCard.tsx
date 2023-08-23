2982b2602e1a0a543442fcb3563363d650a0a1b7
Yu Muramatsu <me@murayu.com>
2023-08-23 15:20:23 +0900

    fix: カード内で画像が形を保つ/文字が見切れないよう修正

--- src/components/common/MainCard.tsx
+++ src/components/common/MainCard.tsx
@@ -8,36 +8,47 @@ type propsType = {
 
 export const MainCard = (props: propsType) => {
   return (
-    <Card sx={{ display: 'flex', width: '100vw' }}>
-      <CardMedia
-        image={props.image}
-        title="画像"
-        sx={{
-          height: 75,
-          width: '20vw',
-          objectFit: 'cover',
-          padding: 2,
-        }}
-      />
-      <Box sx={{ display: 'flex', flexDirection: 'column', width: '80vw' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h6">
-            {props.title}
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            color="text.secondary"
-            component="div"
-            sx={{
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-            }}
-          >
-            {props.detail}
-          </Typography>
-        </CardContent>
-      </Box>
-    </Card>
+    <Box marginBottom={1}>
+      <Card sx={{ display: 'flex' }}>
+        <CardMedia
+          image={props.image}
+          title="画像"
+          sx={{
+            height: 75,
+            width: 75,
+            minWidth: 75,
+            objectFit: 'cover',
+            padding: 2,
+          }}
+        />
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography
+              component="div"
+              variant="h6"
+              sx={{
+                width: '164px',
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+              }}
+            >
+              {props.title}
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              component="div"
+              sx={{
+                width: '164px',
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+              }}
+            >
+              {props.detail}
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </Box>
   );
 };
