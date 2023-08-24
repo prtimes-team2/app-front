bc6527e328a928b3c99b2d0cdffd00dbc20aa23d
Riku Usui <rick.2000u@gmail.com>
2023-08-24 12:12:46 +0900

    feat: imported getting city api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import { Main } from './components/Main';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const App = () => {
-  return <Main />;
+  const [cliant] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={cliant}>
+      <Main />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,10 @@
 import { Router } from '../routes/Router';
+import Register from '../pages/Register';
 
 export const Main = () => {
   return (
     <main>
+      <Register />
       <Router />
     </main>
   );
