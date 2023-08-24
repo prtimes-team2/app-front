f6955d5c395b97ba4c85c5dad9d0de53ba4e25cc
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-08-24 11:45:30 +0900

    feat: create coinlog page

---
+++ src/pages/CoinLog.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContexts';
+
+const AppCoinLog = () => {
+  const { coinLogs } = useContext(AuthContext);
+  return (
+    <div className="App">
+      {/* コインログをリスト形式で表示 */}
+      <ul>
+        {coinLogs.map((coinLog) => {
+          // todo - 日付の表示
+          return <li key={coinLog.id}>{coinLog.amount}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default AppCoinLog;
