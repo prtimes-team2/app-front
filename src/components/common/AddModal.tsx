0199590f765325be0ec48a9794a55f2414753144
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-08-25 05:39:29 +0900

    feat: 投稿後にアラートで獲得枚数を表示する

--- src/components/common/AddModal.tsx
+++ src/components/common/AddModal.tsx
@@ -7,6 +7,7 @@ import {
   Modal,
   Select,
   SelectChangeEvent,
+  Stack,
   Typography,
 } from '@mui/material';
 import { useContext, useState } from 'react';
@@ -33,6 +34,8 @@ const style = {
 
 export const AddModal = (prop: Props) => {
   const [age, setAge] = useState('place');
+  const [amount, setAmount] = useState(0);
+  const [isResult, setIsResult] = useState(false);
 
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value);
@@ -40,49 +43,85 @@ export const AddModal = (prop: Props) => {
   const { profile } = useContext(AuthContext);
   return (
     <Modal open={prop.open} onClose={prop.handleClose}>
-      <Box sx={style} display={'block'}>
-        <Box alignItems={'center'} display={'flex'}>
-          <Avatar
-            alt={profile ? profile.displayName : 'no profile'}
-            src={profile && profile.pictureUrl ? profile.pictureUrl : ''}
-            sx={{
-              width: 28,
-              height: 28,
-              marginRight: 1,
-            }}
-          />
-          <FormControl
-            sx={{
-              m: 1,
-              minWidth: 120,
-              marginLeft: 'auto',
-              alignItems: 'center',
-            }}
-            size="small"
-          >
-            <InputLabel id="small-label"></InputLabel>
-            <Select
-              labelId="small-label"
-              id="small"
-              value={age}
-              label="Age"
-              onChange={handleChange}
+      <Stack>
+        {isResult ? (
+          <Box sx={style}>
+            <Typography variant="h5" component="h2">
+              投稿が完了しました
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {amount}コインの報酬を獲得しました
+            </Typography>
+            {/* 閉じるボタン */}
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginTop: '1rem',
+              }}
             >
-              <MenuItem value="place">場所</MenuItem>
-              <MenuItem value="question">質問</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Box display={'flex'}>
-          {age === 'place' ? (
-            <Place />
-          ) : age === 'question' ? (
-            <Question />
-          ) : (
-            <Typography>投稿方法を選択してください</Typography>
-          )}
-        </Box>
-      </Box>
+              <button
+                onClick={() => {
+                  prop.handleClose();
+                }}
+              >
+                閉じる
+              </button>
+            </Box>
+          </Box>
+        ) : (
+          <Stack>
+            <Box sx={style} display={'block'}>
+              <Box alignItems={'center'} display={'flex'}>
+                <Avatar
+                  alt={profile ? profile.displayName : 'no profile'}
+                  src={profile && profile.pictureUrl ? profile.pictureUrl : ''}
+                  sx={{
+                    width: 28,
+                    height: 28,
+                    marginRight: 1,
+                  }}
+                />
+                <FormControl
+                  sx={{
+                    m: 1,
+                    minWidth: 120,
+                    marginLeft: 'auto',
+                    alignItems: 'center',
+                  }}
+                  size="small"
+                >
+                  <InputLabel id="small-label"></InputLabel>
+                  <Select
+                    labelId="small-label"
+                    id="small"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="place">場所</MenuItem>
+                    <MenuItem value="question">質問</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <Box display={'flex'}>
+                {age === 'place' ? (
+                  <Place
+                    handleResult={(value, amount) => {
+                      setIsResult(value);
+                      setAmount(amount);
+                    }}
+                  />
+                ) : age === 'question' ? (
+                  <Question />
+                ) : (
+                  <Typography>投稿方法を選択してください</Typography>
+                )}
+              </Box>
+            </Box>
+          </Stack>
+        )}
+      </Stack>
     </Modal>
   );
 };
