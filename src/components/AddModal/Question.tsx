296a3d98ca749eb44519a38ce1280c70b867ef68
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-08-25 11:25:58 +0900

    feat: push question

--- src/components/AddModal/Question.tsx
+++ src/components/AddModal/Question.tsx
@@ -1,6 +1,6 @@
+import liff from '@line/liff';
 import * as React from 'react';
 import { Filter } from '../common/Filter';
-import liff from '@line/liff';
 
 import { TextareaAutosize } from '@mui/base/TextareaAutosize';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
@@ -47,8 +47,69 @@ export const Question = () => {
       return;
     }
 
+    function splitAddress(address: string): string[] {
+      const prefectures = [
+        '北海道',
+        '青森県',
+        '岩手県',
+        '宮城県',
+        '秋田県',
+        '山形県',
+        '福島県',
+        '茨城県',
+        '栃木県',
+        '群馬県',
+        '埼玉県',
+        '千葉県',
+        '東京都',
+        '神奈川県',
+        '新潟県',
+        '富山県',
+        '石川県',
+        '福井県',
+        '山梨県',
+        '長野県',
+        '岐阜県',
+        '静岡県',
+        '愛知県',
+        '三重県',
+        '滋賀県',
+        '京都府',
+        '大阪府',
+        '兵庫県',
+        '奈良県',
+        '和歌山県',
+        '鳥取県',
+        '島根県',
+        '岡山県',
+        '広島県',
+        '山口県',
+        '徳島県',
+        '香川県',
+        '愛媛県',
+        '高知県',
+        '福岡県',
+        '佐賀県',
+        '長崎県',
+        '熊本県',
+        '大分県',
+        '宮崎県',
+        '鹿児島県',
+        '沖縄県',
+        // 誤記を考慮する場合は都道府県名のバリエーション(例："京都"、“大阪”など)も追加できます
+      ];
+
+      for (const pre of prefectures) {
+        if (address.startsWith(pre)) {
+          return [pre, address.slice(pre.length)];
+        }
+      }
+
+      throw new Error(`Invalid address: ${address}`);
+    }
+
     // addressは滋賀県大津市のように都道府県と市区町村が入っているaddressを都道府県のいずれかでsplitしてprefectureとcityに分ける
-    const [prefecture, city] = address.split(/(都|道|府|県)/);
+    const [prefecture, city] = splitAddress(address);
     console.log(prefecture);
     console.log(city);
 
